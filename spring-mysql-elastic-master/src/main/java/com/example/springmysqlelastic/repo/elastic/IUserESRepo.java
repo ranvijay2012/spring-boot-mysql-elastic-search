545988676ba7f522f61e2c3c0f2b70a09ea90b40
Ranvijay Singh <ranvijsysingh2012@gmail.com>
2023-04-20 23:14:11 +0530

    Add project

--- spring-mysql-elastic-master/src/main/java/com/example/springmysqlelastic/repo/elastic/IUserESRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.springmysqlelastic.repo.elastic;
-
-import com.example.springmysqlelastic.model.UserModel;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface IUserESRepo extends ElasticsearchRepository<UserModel, Long> {
-}
--- spring-mysql-elastic-master/src/main/java/com/example/springmysqlelastic/service/ISearchService.java
+++
@@ -1,9 +0,0 @@
-package com.example.springmysqlelastic.service;
-
-import com.example.springmysqlelastic.utils.ResultQuery;
-
-import java.io.IOException;
-
-public interface ISearchService {
-    ResultQuery searchFromQuery(String query) throws IOException;
-}
